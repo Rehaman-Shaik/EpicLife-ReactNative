3952f1da7d683fd9ebcbd6da45d55b28dd35b27f
Rehaman <shaik.rehamanrp@gmail.com>
2024-08-19 22:39:27 +0530

    post creation componnet this is

--- components/feed/post.jsx
+++ components/feed/post.jsx
@@ -5,7 +5,13 @@ function Post() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Text style={styles.headerText}>Rehaman</Text>
+                <View style={styles.profileContainer}>
+                    <View style={styles.profilePic}>
+                        {// Profile pic placeholder
+                        }
+                    </View>
+                    <Text style={styles.headerText}>Rehaman</Text>
+                </View>
                 <Text style={styles.headerText}>19/07/2024</Text>
             </View>
             <View style={styles.imageContainer}>
@@ -27,18 +33,32 @@ const styles = StyleSheet.create({
         flexDirection: "column",
         borderWidth: 1,
         borderColor: "black",
-        borderRadius: 5,
+        borderRadius: 10,
+        borderWidth: 1.5,
         marginTop: 10,
-        marginBottom: 10,
+        marginBottom: 5,
         overflow: 'hidden',
     },
     header: {
         flexDirection: "row",
         justifyContent: "space-between",
+        alignItems: "center",
         padding: 10,
         backgroundColor: "#f0f0f0",
         borderBottomWidth: 1,
         borderBottomColor: "#ccc",
+    },
+    profileContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    profilePic: {
+        height: 25,
+        width: 25,
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 50,
+        marginRight: 8, // Adds space between the profile picture and the name
     },
     headerText: {
         fontWeight: "bold",
