0f0be7c04a89645baebafdc9cc2043c8c2402e28
Rehaman <shaik.rehamanrp@gmail.com>
2024-08-21 21:12:37 +0530

    actual notification page with basic or test layout

---
+++ app/Notifications.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const Notifications = () => {
+  return (
+    <View style={[styles.container, {
+      // Try setting `flexDirection` to `"column"`.
+      flexDirection: "column"
+    }]}>
+      <View style={{ flex: 3, backgroundColor: "red" }} />
+      <View style={{ flex: 1, backgroundColor: "orange" }} />
+      <View style={{ flex: 2, backgroundColor: "green" }} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  }
+});
+
+export default Notifications;
