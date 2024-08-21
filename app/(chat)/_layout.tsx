ec66503ff46a1cb8fcdbe1b84c505f9324043de8
Rehaman <shaik.rehamanrp@gmail.com>
2024-08-21 23:03:07 +0530

    fix the arrangement of the chat

--- app/(chat)/_layout.tsx
+++ app/(chat)/_layout.tsx
@@ -3,8 +3,9 @@ import { Tabs } from "expo-router";
 export default function HomeTabs() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="friendsList" options={{ title: "friendsList" }} />
-      <Tabs.Screen name="addFriends" options={{ title: "addFriends" }} />
+      <Tabs.Screen name="chat" options={{ title: "Chat" }} />
+      <Tabs.Screen name="friendsList" options={{ title: "Friends List" }} />
+      <Tabs.Screen name="addFriends" options={{ title: "Add Friends" }} />
     </Tabs>
   );
 }
