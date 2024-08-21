6c5761dbfa69c338ed9507a429e5b3cf61928917
Rehaman <shaik.rehamanrp@gmail.com>
2024-08-21 23:00:10 +0530

    added the some icons for main tabs with a suitable package

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,41 @@
 import { Tabs } from "expo-router";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default function HomeTabs() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
-      <Tabs.Screen name="todo" options={{ title: "To-Do" }} />
-      <Tabs.Screen name="diary" options={{ title: "Diary" }} />
-      <Tabs.Screen name="finance" options={{ title: "Finance" }} />
-      <Tabs.Screen name="watchList" options={{ title: "WatchList" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      <Tabs.Screen name="home" options={{
+        title: "Home", tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }} />
+      <Tabs.Screen name="todo" options={{
+        title: "To-Do", tabBarIcon: ({ color, size }) => (
+          <FontAwesome5 name="tasks" color={color} size={size} />
+        ),
+      }} />
+      <Tabs.Screen name="diary" options={{
+        title: "Diary", tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="book-edit" color={color} size={size} />
+        )
+      }} />
+      <Tabs.Screen name="finance" options={{
+        title: "Finance", tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="finance" color={color} size={size} />
+        )
+      }} />
+      <Tabs.Screen name="watchList" options={{
+        title: "WatchList", tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="list-status" color={color} size={size} />
+        )
+      }} />
+      <Tabs.Screen name="profile" options={{
+        title: "Profile", tabBarIcon: ({ color, size }) => (
+          <MaterialIcons name="person" color={color} size={size} />
+        )
+      }} />
     </Tabs>
   );
 }
