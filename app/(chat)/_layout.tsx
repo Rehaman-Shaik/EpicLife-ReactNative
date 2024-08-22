25d5584d24b8970de869258bc6151fd527bb9d53
Rehaman <shaik.rehamanrp@gmail.com>
2024-08-22 21:43:24 +0530

    build verion 0.0.8

--- app/(chat)/_layout.tsx
+++ app/(chat)/_layout.tsx
@@ -1,11 +1,18 @@
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Tabs } from "expo-router";
 
 export default function HomeTabs() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="chat" options={{ title: "Chat" }} />
-      <Tabs.Screen name="friendsList" options={{ title: "Friends List" }} />
-      <Tabs.Screen name="addFriends" options={{ title: "Add Friends" }} />
+      <Tabs.Screen name="chat" options={{ title: "Chat", tabBarIcon: ({ color, size }) => (
+          <Ionicons name="chatbubble-ellipses-sharp" color={color} size={size} />
+        ) }} />
+      <Tabs.Screen name="friendsList" options={{ title: "Friends List", tabBarIcon: ({ color, size }) => (
+          <Ionicons name="people-sharp" color={color} size={size} />
+        ) }} />
+      <Tabs.Screen name="addFriends" options={{ title: "Add Friends", tabBarIcon: ({ color, size }) => (
+          <Ionicons name="person-add-sharp" color={color} size={size} />
+        ) }} />
     </Tabs>
   );
 }
