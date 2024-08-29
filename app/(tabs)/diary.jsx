ab66cf3f36af4ad87f27ff3ddf81d60a43ea681e
Rehaman <shaik.rehamanrp@gmail.com>
2024-08-30 00:10:42 +0530

    build version 0.0.9

--- app/(tabs)/diary.jsx
+++ app/(tabs)/diary.jsx
@@ -1,42 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, SafeAreaView, StyleSheet, Text, TouchableOpacity, ScrollView, TextInput, Button } from 'react-native';
-import { HomeLinkButton } from '@/components/Button';
-import diaryEntries from '../../components/diary/entrylist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const adddiaryItem = async (newEntry) => {
+    console.log(JSON.stringify(newEntry));
+    const userToken = await AsyncStorage.getItem('authToken');
+    try {
+        const response = await fetch(`https://rehamanshaikofficial.xyz/diary/${userToken}/add`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newEntry),
+        });
+        if (response.ok) {
+            console.log("Item added successfully");
+        } else {
+            console.error("Failed to add item:", response.status);
+        }
+    } catch (error) {
+        console.error("Error adding to-do item:", error.message);
+    }
+}
+
+async function getData(setDiaryEntriesReact) {
+    const userToken = await AsyncStorage.getItem('authToken');
+    const url = `https://rehamanshaikofficial.xyz/diary/${userToken}/all`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        setDiaryEntriesReact(data);
+    } catch (error) {
+        console.error("Error fetching to-do items:", error.message);
+    }
+}
 
 export default function diary() {
     const [isPressed, setIsPressed] = useState(null);
-    const [diaryEntriesReact, setDiaryEntriesReact] = useState(diaryEntries);
+    const [diaryEntriesReact, setDiaryEntriesReact] = useState([]);
     const [text, setText] = useState('');
 
+    useEffect(() => {
+        getData(setDiaryEntriesReact);
+    }, []);
+    
     const handlePress = (id) => {
         setIsPressed(prevValue => (prevValue === id ? null : id));
     };
 
-    const handlePressInputButton = () => {
+    const handlePressInputButton = async () => {
         if (text.trim() !== '') {
             const newEntry = {
-                id: diaryEntriesReact.length + 1,  // Ensure the id is unique and incremental
+                id : diaryEntriesReact.length+1,
                 time: new Date().toLocaleString(),  // Properly formatted date and time
                 text: text,
             };
             setDiaryEntriesReact(prevItems => [...prevItems, newEntry]);
             setText('');
+            adddiaryItem(newEntry);
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            await getData(setDiaryEntriesReact);
         }
     };
 
-    const renderDiaryEntry = (entry) => {
-        const displayText = isPressed === entry.id ? entry.text : entry.text.slice(0, 130) + '...';
+    const renderDiaryEntry = (entry, index) => {
+        var index = index + 1
+        const displayText = isPressed === index ? entry.text : entry.text.slice(0, 130) + '...';
         return (
-            <TouchableOpacity key={entry.id} onPress={() => handlePress(entry.id)} style={isPressed === entry.id ? styles.pressedDiary : styles.diary}>
+            <TouchableOpacity key={index} onPress={() => handlePress(index)} style={isPressed === index ? styles.pressedDiary : styles.diary}>
                 <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
-                    <Text style={styles.title}>{entry.id}</Text>
+                    <Text style={styles.title}>{index}</Text>
                     <Text style={styles.time}>{entry.time}</Text>
                 </View>
                 <Text style={styles.text}>{displayText}</Text>
             </TouchableOpacity>
         );
     };
-
+    
     return (
         <SafeAreaView style={styles.topContainer}>
             <ScrollView>
@@ -53,9 +95,9 @@ export default function diary() {
                 />
                 <Button title="Add" onPress={handlePressInputButton} />
             </View>
-
         </SafeAreaView>
     );
+    
 }
 
 const styles = StyleSheet.create({
@@ -63,12 +105,12 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 16,
         backgroundColor: '#f5f5f5',
-        marginTop:15
+        marginTop: 15
     },
     diary: {
         borderWidth: 1,
         borderColor: "black",
-        borderRadius: 5,
+        borderRadius: 10,
         height: 100,
         width: "100%",
         padding: 10,
@@ -78,7 +120,7 @@ const styles = StyleSheet.create({
     pressedDiary: {
         borderWidth: 1,
         borderColor: "black",
-        borderRadius: 5,
+        borderRadius: 10,
         height: 300,
         width: "100%",
         padding: 10,
@@ -100,7 +142,7 @@ const styles = StyleSheet.create({
     input: {
         borderWidth: 1,
         borderColor: "black",
-        borderRadius: 5,
+        borderRadius: 10,
         flex: 2,
         marginRight: 5,
         height: 40,
--- app/(tabs)/watchList.jsx
+++ app/(tabs)/watchList.jsx
@@ -1,140 +1,221 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, ScrollView, View, Text, StyleSheet, TextInput, Button, TouchableOpacity } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const initialMediaList = [
-    { id: 1, name: "Red Dead Redemption 2", isPlayed: false, mediaType: "game" },
-    { id: 2, name: "Game of Thrones", isPlayed: true, mediaType: "series" },
-    { id: 3, name: "Interstellar", isPlayed: false, mediaType: "movie" },
-    // Add more media here
+const addWatchItem = async (newEntry) => {
+  console.log(JSON.stringify(newEntry));
+  const userToken = await AsyncStorage.getItem('authToken');
+  try {
+    const response = await fetch(`https://rehamanshaikofficial.xyz/watch-list/${userToken}/add`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newEntry),
+    });
+    if (response.ok) {
+      console.log("Item added successfully");
+    } else {
+      console.error("Failed to add item:", response.status);
+    }
+  } catch (error) {
+    console.error("Error adding to-do item:", error.message);
+  }
+}
+
+async function getData(setMediaList) {
+  const userToken = await AsyncStorage.getItem('authToken');
+  const url = `https://rehamanshaikofficial.xyz/watch-list/${userToken}/all`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const data = await response.json();
+    setMediaList(data);
+  } catch (error) {
+    console.error("Error fetching to-do items:", error.message);
+  }
+}
+
+const mediaTypeOptions = [
+  { label: 'Game', value: 'game' },
+  { label: 'Series', value: 'series' },
+  { label: 'Movie', value: 'movie' },
+  { label: 'Book', value: 'book' },
+  // Add more media types if needed
 ];
 
 function MediaList() {
-    const [mediaName, setMediaName] = useState("");
-    const [mediaType, setMediaType] = useState("");
-    const [mediaList, setMediaList] = useState(initialMediaList);
+  const [mediaName, setMediaName] = useState("");
+  const [mediaType, setMediaType] = useState(null);
+  const [mediaList, setMediaList] = useState([]);
 
-    const toggleMediaStatus = (id) => {
-        setMediaList(prevList =>
-            prevList.map(media =>
-                media.id === id ? { ...media, isPlayed: !media.isPlayed } : media
-            )
-        );
-    };
+  // This useEffect will only run once after the component mounts
+  useEffect(() => {
+    getData(setMediaList);
+  }, []); // Empty dependency array ensures it runs only once
 
-    const addMedia = () => {
-        if (mediaName.trim() === "" || mediaType.trim() === "") return;
-        const newMedia = {
-            id: mediaList.length + 1,
-            name: mediaName,
-            isPlayed: false,
-            mediaType: mediaType.toLowerCase(),
-        };
-        setMediaList(prevList => [...prevList, newMedia]);
-        setMediaName("");
-        setMediaType("");
-    };
-
-    const renderMedia = (medias, playedStatus) => {
-        return medias.filter(media => media.isPlayed === playedStatus).map(media => (
-            <TouchableOpacity key={media.id} onPress={() => toggleMediaStatus(media.id)}>
-                <MediaCard id={media.id} name={media.name} mediaType={media.mediaType} />
-            </TouchableOpacity>
-        ));
-    };
-
-    function MediaCard({ id, name, mediaType }) {
-        return (
-            <View style={styles.card}>
-                <Text style={styles.mediaId}>{id}</Text>
-                <Text style={styles.mediaName}>{name}</Text>
-                <Text style={styles.mediaType}>{mediaType.charAt(0).toUpperCase() + mediaType.slice(1)}</Text>
-            </View>
-        );
-    }
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.section}>
-                <Text style={styles.sectionTitle}>Media to Watch/Play</Text>
-                <ScrollView>
-                    {renderMedia(mediaList, false)}
-                </ScrollView>
-            </View>
-            <View style={styles.section}>
-                <Text style={styles.sectionTitle}>Watched/Played Media</Text>
-                <ScrollView>
-                    {renderMedia(mediaList, true)}
-                </ScrollView>
-            </View>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Add media name'
-                    value={mediaName}
-                    onChangeText={setMediaName}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder='Enter media type (game/series/movie)'
-                    value={mediaType}
-                    onChangeText={setMediaType}
-                />
-                <Button title="Add" onPress={addMedia} />
-            </View>
-        </SafeAreaView>
+  const toggleMediaStatus = (id) => {
+    setMediaList(prevList =>
+      prevList.map(media =>
+        media.id === id ? { ...media, isPlayed: !media.isPlayed } : media
+      )
     );
+  };
+
+  const addMedia = async () => {
+    if (mediaName.trim() === "" || !mediaType) return;
+    const newMedia = {
+      id: mediaList.length + 1,
+      name: mediaName,
+      isPlayed: false,
+      mediaType,
+    };
+    setMediaList(prevList => [...prevList, newMedia]);
+    setMediaName("");
+    setMediaType(null);
+    await addWatchItem(newMedia);
+    // Update media list after adding the new item
+    await getData(setMediaList);
+  };
+
+  const renderMedia = (medias, playedStatus) => {
+    return medias.filter(media => media.isPlayed === playedStatus).map(media => (
+      <TouchableOpacity key={media.id} onPress={() => toggleMediaStatus(media.id)}>
+        <MediaCard id={media.id} name={media.name} mediaType={media.mediaType} />
+      </TouchableOpacity>
+    ));
+  };
+
+  function MediaCard({ id, name, mediaType }) {
+    return (
+      <View style={styles.card}>
+        <Text style={styles.mediaId}>{id}</Text>
+        <Text style={styles.mediaName}>{name}</Text>
+        <Text style={styles.mediaType}>{mediaType.charAt(0).toUpperCase() + mediaType.slice(1)}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Media to Watch/Play</Text>
+        <ScrollView>
+          {renderMedia(mediaList, false)}
+        </ScrollView>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Watched/Played Media</Text>
+        <ScrollView>
+          {renderMedia(mediaList, true)}
+        </ScrollView>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Add media name'
+          value={mediaName}
+          onChangeText={setMediaName}
+        />
+        <Dropdown
+          style={styles.dropdown}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={mediaTypeOptions}
+          labelField="label"
+          valueField="value"
+          placeholder="Select media type"
+          value={mediaType}
+          onChange={item => setMediaType(item.value)}
+        />
+        <Button title="Add" onPress={addMedia} />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-        backgroundColor: '#f5f5f5',
-        marginTop:15
-    },
-    section: {
-        marginBottom: 20,
-    },
-    sectionTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    card: {
-        padding: 10,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-        backgroundColor: '#fff',
-        marginBottom: 10,
-    },
-    mediaId: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    mediaName: {
-        fontSize: 14,
-        color: '#555',
-    },
-    mediaType: {
-        fontSize: 12,
-        color: '#888',
-        fontStyle: 'italic',
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 20,
-    },
-    input: {
-        flex: 1,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-        paddingHorizontal: 8,
-        backgroundColor: '#fff',
-        marginRight: 10,
-    },
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#f5f5f5',
+    marginTop: 15,
+  },
+  section: {
+    marginBottom: 20,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  card: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    marginBottom: 10,
+  },
+  mediaId: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  mediaName: {
+    fontSize: 14,
+    color: '#555',
+  },
+  mediaType: {
+    fontSize: 12,
+    color: '#888',
+    fontStyle: 'italic',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    paddingHorizontal: 8,
+    backgroundColor: '#fff',
+    marginRight: 10,
+    height:40
+  },
+  dropdown: {
+    flex: 1,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 8,
+    backgroundColor: '#fff',
+    marginRight:10
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    color: '#888',
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    color: '#000',
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
 });
 
 export default MediaList;
